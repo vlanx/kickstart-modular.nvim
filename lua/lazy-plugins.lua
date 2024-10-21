0210c50400d9175f14fce3c2528d51d5b5195092
Tiago Barros <tiagobarros99@ua.pt>
2024-10-21 10:49:52 +0100

    removed yet another border

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -65,7 +65,7 @@ require('lazy').setup({
   },
   defaults = {},
   ui = {
-    border = 'single',
+    border = 'none',
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
