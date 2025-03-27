81d109196945280e934c31ab0017fb3e3f447f21
Tiago Barros <tiagobarros99@ua.pt>
2025-03-27 11:21:10 +0000

    sneak changes

--- lua/custom/plugins/sneak.lua
+++ lua/custom/plugins/sneak.lua
@@ -1,11 +1,9 @@
 return {
   'justinmk/vim-sneak',
-  config = function()
+  init = function()
     -- Change default vim-sneak mapping from 's' to '\'
-    vim.keymap.set({ 'n', 'x' }, '+', '<Plug>Sneak_s', { desc = 'Vim Sneak keybind forwards' })
-    vim.keymap.set({ 'n', 'x' }, '=', '<Plug>Sneak_S', { desc = 'Vim Sneak keybind forwards' })
-    vim.keymap.set({ 'n', 'x' }, '\\', '<Plug>SneakLabel_s', { desc = 'Vim Sneak Label keybind forwards' })
-    vim.keymap.set({ 'n', 'x' }, '|', '<Plug>SneakLabel_S', { desc = 'Vim Sneak Label keybind backwards' })
+    vim.keymap.set({ 'n', 'x' }, '\\', '<Plug>Sneak_s', { desc = 'Vim Sneak keybind forwards' })
+    vim.keymap.set({ 'n', 'x' }, '|', '<Plug>Sneak_S', { desc = 'Vim Sneak keybind forwards' })
 
     -- Change f/F and t/T commands to the vim-sneak enhanced ones
     vim.keymap.set({ 'n', 'x' }, 'f', '<Plug>Sneak_f', { desc = 'Vim Sneak keybind forwards' })
